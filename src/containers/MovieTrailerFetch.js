858aec69df9d5f3e50d38638e955671b10c3d9ed
Ivan Filho <ivanslf@gmail.com>
2018-11-27 00:44:33 -0300

    feat: add trailer

---
+++ src/containers/MovieTrailerFetch.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+const MOVIE_API = 'https://api.themoviedb.org/3/movie'
+const URL_SUFFIX = `videos?api_key=${process.env.REACT_APP_API_KEY}`
+
+class MovieTrailerFetch extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount () {
+    window.fetch(`${MOVIE_API}/${this.props.id}/${URL_SUFFIX}`)
+      .then(response => response.json())
+      .then(data => this.setState({
+        loading: false,
+        data: data.results
+      }))
+      .catch(() => this.setState({
+        loading: false,
+        error: true
+      }))
+  }
+
+  render () {
+    if (this.state.loading) {
+      return 'Aguarde...'
+    }
+
+    if (this.state.error) {
+      return 'Oops! Algo errado aconteceu.'
+    }
+
+    const videos = this.state.data.filter(_ =>
+      _.type === 'Trailer' && _.site === 'YouTube')
+
+    if (!videos.length) {
+      return ''
+    }
+
+    return (
+      <div style={
+        {
+          position: 'relative',
+          paddingTop: '56.25%'
+        }
+      }>
+        <iframe
+          src={`http://www.youtube.com/embed/${videos[0].key}`}
+          style={
+            {
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              border: 0
+            }
+          }
+        />
+      </div>
+    )
+  }
+}
+
+export default MovieTrailerFetch
